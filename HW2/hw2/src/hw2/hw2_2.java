40a35994e4cbf748c8045c9ef87c6c3ea770e9bd
122214206 <160557895+122214206@users.noreply.github.com>
2024-03-05 21:28:20 +0800

    Update hw2_2.java

--- HW2/hw2/src/hw2/hw2_2.java
+++ HW2/hw2/src/hw2/hw2_2.java
@@ -6,14 +6,11 @@ import java.util.Random;
 
 public class hw2_2 {
 
-
-
 	public static void main(String[] args) {
 
 		Random rand = new Random();
 
 		int ans=rand.nextInt(100);
-
 		int min=0;
 		int max=100;
 		int guess=0;
@@ -22,28 +19,19 @@ public class hw2_2 {
 
 		while(true) {
 
+			System.out.println("目前範圍: " + min + " - " + max);
 			System.out.print("請輸入一個數字");
-
 			guess=sc.nextInt();
 
-			
-
-			int validate_num=ans-guess;
-
-			if(guess>0) {
-
-				System.out.println("你猜的答案太小了");
-
-			}else if(validate_num<0) {
-
+			if(guess>ans) {
 				System.out.println("你猜的答案太大了");
-
+				max=guess-1;
+			}else if(guess<ans) {
+				System.out.println("你猜的答案太小了");
+				min=guess+1;
 			}else{
-
 				System.out.println("你猜對答案了");
-
 				break;
-
 			}
 
 		}
